1fa35a57e2ff0ff9f48452f89793c0f06d5b3b9a
Polbv <pobar24@student.sdu.dk>
2024-12-18 13:42:50 +0100

    Update HOST IP address in Storytelling_text_sync

--- python/samples/Storytelling_text_sync.py
+++ python/samples/Storytelling_text_sync.py
@@ -242,7 +242,7 @@ async def run(client: RTClient, instructions_file_path: str, out_dir: str):
         log("Configuring Session...")
         await client.configure(instructions=instructions,voice='echo')
         client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        HOST="192.168.218.63"
+        HOST="192.168.120.63"
         PORT=12345
         client_socket.connect((HOST,PORT))
         log("Done")
